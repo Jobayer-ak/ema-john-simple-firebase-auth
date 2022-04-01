ec5edc9c81a49dc303d8b68cb40c1ec3ca4cf7a0
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-01 11:18:06 +0600

    working with router

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import "./Cart.css"
+import React from "react";
+import "./Cart.css";
 
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+  const { cart } = props;
 
-    let total = 0;
-    let shipping = 0;
-    let quantity = 0;
-    for (const product of cart) {
-        quantity += product.quantity;
-        total = total + product.price *product.quantity;
-        shipping += product.shipping;
-    }
+  console.log(props.children);
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity += product.quantity;
+    total = total + product.price * product.quantity;
+    shipping += product.shipping;
+  }
 
-    const tax = parseFloat((total * 0.1).toFixed(2));
-    const grandTotal = total + shipping + tax;
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
 
-    return (
-        <div className='cart'>
-            <h3>Order summary</h3>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${total}</p>
-            <p>Total Shipping: ${shipping}</p>
-            <p>Tax: ${tax}</p>
-            <h5>Grand Total:${ grandTotal.toFixed(2)} </h5>
-        </div>
-    );
+  return (
+    <div className="cart">
+      <h3>Order summary</h3>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h5>Grand Total:${grandTotal.toFixed(2)} </h5>
+      {props.children}
+    </div>
+  );
 };
 
 export default Cart;
