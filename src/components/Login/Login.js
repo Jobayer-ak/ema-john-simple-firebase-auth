d54c7557591e8d28a5664e6f2adf5af721958241
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-18 00:04:32 +0600

    added handle sign in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className="form-container">
       <div>
         <h2 className="form-title">Login</h2>
-        <form action="#">
+        <form onSubmit={handleUserSignIn} action="#">
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" required />
+            <input
+              onClick={handleEmailBlur}
+              type="email"
+              name="email"
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" required />
+            <input
+              onClick={handlePasswordBlur}
+              type="password"
+              name="password"
+              required
+            />
           </div>
           <input className="form-submit" type="submit" value="Login" />
         </form>
